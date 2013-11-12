e9d8fbadc9f2a044d64d806163b3421d4bc2aac6
tomassburton <tomas.nahl@gmail.com>
2013-11-12 13:17:12 +0100

    první verze šablon

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var $j = jQuery.noConflict();
+$j('#slider1').bxSlider({
+  captions: true,
+  auto: true
+});
+
+var $p = jQuery.noConflict();
+$p('#slider2').bxSlider({
+  captions: true,
+  auto: true
+});
