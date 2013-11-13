3fea87b8a4d1a0f5f61f29af09493eb79ffb024e
tomassburton <tomas.nahl@gmail.com>
2013-11-13 23:16:35 +0100

    Dodělávky
    Přidány modal boxy, drobečková navigace, a rozdělaný detail produktu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+var $r = jQuery.noConflict();
+$r(document).ready(function() {
+	$(".modal").fancybox({
+		maxWidth	: 900,
+		maxHeight	: 800,
+		fitToView	: false,
+		width		: '80%',
+		height		: '80%',
+		autoSize	: true,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+});
+
 var $j = jQuery.noConflict();
 $j('#slider1').bxSlider({
   captions: true,
