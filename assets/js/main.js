5e8388100b17e08340a641f4b0685b5367478527
tomassburton <tomas.nahl@gmail.com>
2013-11-14 22:52:06 +0100

    Opraveny některé bugy, kategorie

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,30 @@ $r(document).ready(function() {
 	});
 });
 
+$('ul.tabs').each(function(){
+  var $active, $content, $links = $(this).find('a');
+  $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+  $active.addClass('active');
+  $content = $($active.attr('href'));
+
+  $links.not($active).each(function () {
+    $($(this).attr('href')).hide();
+  });
+
+  $(this).on('click', 'a', function(e){
+    $active.removeClass('active');
+    $content.hide();
+
+    $active = $(this);
+    $content = $($(this).attr('href'));
+
+    $active.addClass('active');
+    $content.show();
+
+    e.preventDefault();
+  });
+});
+
 var $j = jQuery.noConflict();
 $j('#slider1').bxSlider({
   captions: true,
@@ -23,4 +47,10 @@ var $p = jQuery.noConflict();
 $p('#slider2').bxSlider({
   captions: true,
   auto: true
+});
+
+var $q = jQuery.noConflict();
+$q('#slider3').bxSlider({
+  captions: true,
+  auto: false
 });
